7f36295c6b4158156000046ae9893f3b8163d06b
Shreyash Thaware <shreyashthaware3@gmail.com>
2023-09-30 04:14:22 +0530

    app js and css

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 /* eslint-disable no-unused-vars */
-import './App.scss';
-import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
-import Home from './Components/Home/Home';
-import Header from './Components/Header/Header';
-
+import "./App.scss";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Components/Home/Home";
+import Header from "./Components/Header/Header";
 
 function App() {
-      return <Router>
-
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>  
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
